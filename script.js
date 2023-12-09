6a8771568cabab06f75bea61d7492777a071c45d
salunkevv <86159929+salunkevv@users.noreply.github.com>
2023-12-09 13:06:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 function createTable() {
-    //Write your code here
-  
+  //Write your code here
+let cn= prompt("Input number of colums");
+let rn=prompt("Input number of rows");
+let table= document.getElementById("myTable");
+for(let i=0;i<rn;i++)
+{
+  let row=document.createElement("tr")
+  for(let j=0;j<cn;j++)
+  {
+    let cell=document.createElement("td");
+    cell.innerText=`Row-${i} Column-${j}`
+    row.append(cell);
+  }
+  table.append(row);
+}
 }
